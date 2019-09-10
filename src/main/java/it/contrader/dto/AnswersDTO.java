444c2d7703aa0f0d69e09ae21f8b1e3afaf52b68
Mariangela92 <dilellamariangela.mdl@gmail.com>
2019-09-10 17:09:09 +0200

    updtoans

---
+++ src/main/java/it/contrader/dto/AnswersDTO.java
@@ -0,0 +1,72 @@
+package it.contrader.dto;
+
+public class AnswersDTO {
+      
+	private int id;
+	
+	private int iduser;
+	
+	private int idquestion;
+	
+	private int answer;
+
+	public AnswersDTO() {
+		super();
+	}
+
+	public AnswersDTO(int id, int iduser, int idquestion, int answer) {
+		super();
+		this.id = id;
+		this.iduser = iduser;
+		this.idquestion = idquestion;
+		this.answer = answer;
+	}
+
+	public AnswersDTO(int iduser, int idquestion, int answer) {
+		super();
+		this.iduser = iduser;
+		this.idquestion = idquestion;
+		this.answer = answer;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getIduser() {
+		return iduser;
+	}
+
+	public void setIduser(int iduser) {
+		this.iduser = iduser;
+	}
+
+	public int getIdquestion() {
+		return idquestion;
+	}
+
+	public void setIdquestion(int idquestion) {
+		this.idquestion = idquestion;
+	}
+
+	public int getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(int answer) {
+		this.answer = answer;
+	}
+
+	@Override
+	public String toString() {
+		return id + "\t" + iduser  + "\t\t " + idquestion + "\t\t " + answer;
+		
+	}
+	
+	
+	
+}
